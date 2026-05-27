dc0b8caf23cb67a46a81bee9aba674f981c50675
jessicawaz <jesswaz05@gmail.com>
2026-05-27 16:44:17 -0400

    add monthly recap cron + api-only backend

--- apps/web/.next/dev/static/chunks/[project]_apps_web_src_pages__error_07xvfw~._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/0nex_next_dist_compiled_0g6k~3.._.js","static/chunks/0nex_next_dist_shared_lib_0l1wrlo._.js","static/chunks/0nex_next_dist_client_005j~f2._.js","static/chunks/0nex_next_dist_06.zp-0._.js","static/chunks/12t3_next_dist_pages__error_0nioisk.js","static/chunks/[next]_entry_page-loader_ts_0rqw6yo._.js","static/chunks/0nex_react-dom_0i2efab._.js","static/chunks/_0x8mz87._.js","static/chunks/[root-of-the-server]__0cafkmv._.js"],
-    source: "entry"
-});
--- apps/web/.next/dev/static/chunks/apps_web_src_pages__error_07xvfw~._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/0fuv_next_dist_compiled_09u67fy._.js","static/chunks/0fuv_next_dist_shared_lib_0dbign0._.js","static/chunks/0fuv_next_dist_client_08kqo5b._.js","static/chunks/0fuv_next_dist_0_euhq~._.js","static/chunks/node_modules_next_dist_pages__error_0nioisk.js","static/chunks/[next]_entry_page-loader_ts_0rqw6yo._.js","static/chunks/0fuv_react-dom_04004tw._.js","static/chunks/_0.ehzun._.js","static/chunks/[root-of-the-server]__0cafkmv._.js"],
-    source: "entry"
-});
