607001b58cbd261d56f041916a4bd821cbe1101c
jessicawaz <jesswaz05@gmail.com>
2026-05-27 16:57:23 -0400

    add clg to cron for verification

--- apps/backend/cron.js
+++ apps/backend/cron.js
@@ -1,6 +1,7 @@
-const cron = require('node-cron');
+const cron = require("node-cron");
 
 cron.schedule("0 9 1 * *", async () => {
+  console.log("Cron scheduler started");
   await fetch("https://jarly-production.up.railway.app/api/v1/monthly-recap", {
     method: "POST",
     headers: {
